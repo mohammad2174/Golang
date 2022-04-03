6deca69b416e092d471ebe2903c6c27493e851d9
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2022-04-03 15:42:16 +0430

    working on the parameters go

---
+++ go-project/09_parameters/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func doubleAt(values []int, i int) {
+	values[i] *= 2
+}
+
+func double(n *int) {
+	*n *= 2
+}
+
+func main() {
+	values := []int{1, 2, 3, 4}
+	doubleAt(values, 2)
+	fmt.Println(values)
+
+	val := 10
+	double(&val)
+	fmt.Println(val)
+}
