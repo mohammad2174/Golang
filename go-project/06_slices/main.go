4241fc03eda7856e6fd0d4691cbde2bebf88991c
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2022-04-03 10:06:12 +0430

    working on the slices go

---
+++ go-project/06_slices/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	foo := []string{"mohammad", "reza", "khorrami"}
+
+	fmt.Printf("foo = %v (type %T)\n", foo, foo)
+
+	fmt.Println(len(foo))
+	
+	fmt.Println(foo[1])
+
+	fmt.Println(foo[1:])
+
+	for i := 0; i < len(foo); i++ {
+		fmt.Println(foo[i])
+	}
+
+	for index := range foo {
+		fmt.Println(index)
+	}
+
+	for index, value := range foo {
+		fmt.Printf("%s at %d\n", value, index)
+	}
+
+	for _, value := range foo {
+		fmt.Println(value)
+	}
+}
