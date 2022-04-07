8acc59169350a1b4389991ed06b423ca7a1ec4a2
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2022-04-07 12:59:47 +0430

    working on the gorountines go

---
+++ go-project/18_goroutines/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+func returnType(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("ERROR: %s\n", err)
+		return
+	}
+	defer resp.Body.Close()
+	ctype := resp.Header.Get("Content-Type")
+	fmt.Printf("%s -> %s\n", url, ctype)
+}
+
+func main() {
+	urls := []string {
+		"https://golang.org",
+		"https://api.github.com",
+		"https://httpbin.org/xml",
+	}
+
+	var wg sync.WaitGroup
+	for _, url := range urls {
+		wg.Add(1)
+		go func(url string) {
+			returnType(url)
+			wg.Done()
+		}(url)
+	}
+	wg.Wait()
+}
